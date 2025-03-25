b503361e68ef507ac4eb0e1c72d193498ad11f19
marlondt-dev <marlon.dt@hotmail.com>
2025-03-25 11:47:39 +0100

    feat(addets): added reset and variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  css: ["~/assets/styles/reset.css"],
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/content'
-  ]
-})
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/image", "@nuxt/content"],
+});
