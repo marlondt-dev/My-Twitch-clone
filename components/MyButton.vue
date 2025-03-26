860aa7f6bf4960efd8e8d82dc3316a19ab9ae737
marlondt-dev <marlon.dt@hotmail.com>
2025-03-26 12:36:59 +0100

    chore(components/TheButton): changed background color

--- components/MyButton.vue
+++ components/MyButton.vue
@@ -12,12 +12,13 @@ defineProps<{
 .btn {
   padding: 0.4em 0.5em;
   font-weight: bold;
-  border-radius: 5%;
+  border-radius: 0.25em;
+  height: 1.87em;
 }
 
 .blue {
   background-color: var(--button-color-blue);
-  color: var(--text-color-white);
+  color: #ffffff;
 }
 
 .gray {
