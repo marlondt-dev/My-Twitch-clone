8869a581d7681a3bc94274257169ff3eb6e24fab
marlondt-dev <marlon.dt@hotmail.com>
2025-03-26 13:20:13 +0100

    feat(components/AsideChat):added new chat component

---
+++ components/chat/AsideChat.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <aside class="chat"></aside>
+</template>
+
+<style lang="scss" scoped>
+.chat {
+  min-width: 16.25em;
+  height: 22.81em;
+  background-color: white;
+}
+</style>
---
+++ components/nabvar/UserNav.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="user-container">
+    <div><img src="../public/button-prime.png" /></div>
+    <div class="user-buttons">
+      <MyButton :class="'gray'">Log in</MyButton>
+      <MyButton :class="'blue'">Sign Up </MyButton>
+      <div><img src="../public/account" /></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.user-container {
+  width: 18em;
+  height: 2.5em;
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+
+  .user-buttons {
+    display: flex;
+    gap: 0.62em;
+  }
+}
+</style>
