29743ccdbde5faa672d86e091a5b3853becdbf01
marlondt-dev <marlon.dt@hotmail.com>
2025-03-26 12:40:03 +0100

    chore(public): added new pictures

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,26 @@
 
 <template>
   <div>
-    <nav class="container">
-      <MyButton :class="'gray'">Log in</MyButton>
-      <MyButton :class="'blue'">Sign Up </MyButton>
+    <nav class="nav-container">
+      <NavChannelName />
+      <NavSearchBar />
+      <UserNav />
     </nav>
     <slot />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.container {
-  max-width: 95em;
+.nav-container {
+  max-width: 95.62em;
   height: 3.12em;
+  margin: auto;
   background-color: var(--bg-color-gray);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.31em 1.25em;
+  box-shadow: 0px 0px 2px 0px #000000e5;
+  box-shadow: 0px 1px 2px 0px #000000e5;
 }
 </style>
